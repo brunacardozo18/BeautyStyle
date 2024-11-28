7e0ff6e077570192aa320dab196738c981062355
Bruna <bruninha.2706sa@gmail.com>
2024-11-28 15:27:51 -0300

    ✨ feat: Cria a conexão com o banco e CORS

---
+++ api/conn.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'sql303.infinityfree.com'; // ou 'localhost'
+$dbName = 'if0_37801800_beautystyle'; // Substitua pelo nome do seu banco
+$user = 'if0_37801800'; // Substitua pelo usuário do banco (geralmente 'root' em localhost)
+$password = 'iP5Bx0CW1Tlgo9'; // Substitua pela senha do usuário (em localhost pode ser vazio)
+
+?>
